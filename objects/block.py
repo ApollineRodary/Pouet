f72f770b3caf6eea1791fea5c58213d0bdf4c724
Apolline Rodary <apollinerodary@gmail.com>
2024-04-16 14:43:40 +0200

    Make comparators squishy

--- objects/block.py
+++ objects/block.py
@@ -3,9 +3,10 @@ import tkinter
 from playsound import playsound
 
 class Block(Object):
-    def __init__(self, x: int, y: int, size: int):
+    def __init__(self, x: int, y: int, size: int, sprites: dict):
         super().__init__(x, y)
         self.size = size
+        self.sprites = sprites
 
     def on_traversed(self, inputs):
         assert len(inputs) == self.size
@@ -14,45 +15,38 @@ class Block(Object):
         super().load(canvas)
         self.image = canvas.create_image(
             self.x,
-            self.y
+            self.y,
+            image = self.sprites["idle"]
         )
+        self.canvas.tag_bind(self.image, "<Enter>", self.on_hover)
+        self.canvas.tag_bind(self.image, "<Leave>", self.on_leave)
         self.canvas.tag_bind(self.image, "<B1-Motion>", self.on_drag)
         self.canvas.tag_bind(self.image, "<ButtonRelease-1>", self.on_drop)
         self.canvas.tag_bind(self.image, "<ButtonPress-1>", self.on_start)
 
+    def on_hover(self, event):
+        self.canvas.itemconfig(self.image, image=self.sprites["hovered"])
+
+    def on_leave(self, event):
+        self.canvas.itemconfig(self.image, image=self.sprites["idle"])
+
     def on_drag(self, event):
         xd = event.x
         yd = event.y
-        if not (xd<0 or xd>1200 or yd<0 or yd>800):
+        if (0<xd<1200 and 0<yd<800):
             self.canvas.coords(self.image, [xd, yd])
 
     def on_drop(self, event):
         playsound('assets/sounds/drop_block.wav', block=False)
-    
+        self.canvas.itemconfig(self.image, image=self.sprites["hovered"])
+
     def on_start(self, event):
         playsound('assets/sounds/drag_block.mp3', block=False)
-
-    def set_command(self, f: callable):
-        self.command = f
-        self.update()
-
-    def on_hover(self, event):
-        self.canvas.itemconfig(
-            self.rectangle,
-            outline = BUTTON_OUTLINE_HOVER_COLOR
+        self.canvas.itemconfig(self.image, image=self.sprites["clicked"])
+        self.canvas.after(
+            100,
+            lambda: self.canvas.itemconfig(
+                self.image,
+                image=self.sprites["held"]
+            )
         )
-        self.canvas.itemconfig(
-            self.label,
-            fill = BUTTON_LABEL_HOVER_COLOR
-        )
-
-    def on_leave(self, event):
-        self.canvas.itemconfig(
-            self.rectangle,
-            outline = BUTTON_OUTLINE_COLOR
-        )
-        self.canvas.itemconfig(
-            self.label,
-            fill = BUTTON_LABEL_COLOR
-        )
-
--- objects/comparator.py
+++ objects/comparator.py
@@ -1,19 +1,20 @@
 from .block import Block
 import tkinter
 
-COMPARATOR_IMAGE = tkinter.PhotoImage(file = "assets/textures/comparator.png")
+SPRITES = {
+    "idle": tkinter.PhotoImage(file="assets/textures/comparator_idle.png"),
+    "hovered": tkinter.PhotoImage(file="assets/textures/comparator_hovered.png"),
+    "held": tkinter.PhotoImage(file="assets/textures/comparator_held.png"),
+    "clicked": tkinter.PhotoImage(file="assets/textures/comparator_clicked.png")
+}
 
 class Comparator(Block):
     def __init__(self, x: int, y: int):
-        super().__init__(x, y, size=2)
+        super().__init__(x, y, size=2, sprites=SPRITES)
 
     def on_traversed(self, inputs):
         super().on_traversed(inputs)
         return [1, 0] if inputs[0] > inputs[1] else [0, 1]
-
-    def load(self, canvas: tkinter.Canvas):
-        super().load(canvas)
-        self.canvas.itemconfig(self.image, image=COMPARATOR_IMAGE)
 
     def unload(self):
         self.canvas.delete(self.image)
